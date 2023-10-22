c64bbfe7a3ce226b80c427d1e4b48c875e86b5b5
Erik <santicasas667@gmail.com>
2023-10-22 10:57:12 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// TODO: Completa tu código aquí ⬇️
+
+
+//Se recomienda no editar ni eliminar la instancia del servidor.
+// Instancia del servidor
+const server = app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}`);
+});
+
+// Exportación del servidor
+module.exports = server;
